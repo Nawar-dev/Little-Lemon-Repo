484133c50a4bbfb98e9548e5421e86806f744fbc
Nawar <kingnawar5@gmail.com>
2026-08-28 12:55:46 +0300

    Add semantic HTML and OGP meta tags

---
+++ my-app/src/Footer.js
@@ -0,0 +1,46 @@
+export default function Footer() {
+  return (
+    <footer>
+      <img src="#" alt="Not Found" />
+      <ul>
+        <li>
+          <h3>Doormat Navigation</h3>
+        </li>
+        <li>
+          <a href="#HomePage">HomePage</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Menu">Menu</a>
+        </li>
+        <li>
+          <a href="#Reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="#OrderOnline">Order Online</a>
+        </li>
+        <li>
+          <a href="#Login">Login</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h3>Contact</h3>
+        </li>
+        <li>Adress</li>
+        <li>Phone Number</li>
+        <li>email</li>
+      </ul>
+      <ul>
+        <li>
+          <h3>Social Media Links</h3>
+        </li>
+        <li>Adress</li>
+        <li>Phone Number</li>
+        <li>email</li>
+      </ul>
+    </footer>
+  );
+}
