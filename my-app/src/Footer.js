7f0c9778ecce9c5e86487197bb5e2fe95e8efb6d
Nawar <kingnawar5@gmail.com>
2026-08-28 14:18:06 +0300

    Add global CSS styles, typography fonts, and responsive layout

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,46 +1,47 @@
 export default function Footer() {
   return (
-    <footer>
-      <img src="#" alt="Not Found" />
-      <ul>
-        <li>
-          <h3>Doormat Navigation</h3>
-        </li>
-        <li>
-          <a href="#HomePage">HomePage</a>
-        </li>
-        <li>
-          <a href="#About">About</a>
-        </li>
-        <li>
-          <a href="#Menu">Menu</a>
-        </li>
-        <li>
-          <a href="#Reservations">Reservations</a>
-        </li>
-        <li>
-          <a href="#OrderOnline">Order Online</a>
-        </li>
-        <li>
-          <a href="#Login">Login</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
+    <footer className="footer-container">
+      <div className="footer-columns">
+        
+        {/* First Column: Logo */}
+        <div className="footer-logo">
+          <img src="Logo.svg" alt="Little Lemon Logo" />
+        </div>
+
+        {/* Second Column : Navigation */}
+        <div className="footer-links">
+          <h3>Navigation</h3>
+          <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Menu">Menu</a></li>
+            <li><a href="#Reservations">Reservations</a></li>
+            <li><a href="#OrderOnline">Order Online</a></li>
+            <li><a href="#Login">Login</a></li>
+          </ul>
+        </div>
+
+        {/* Third Column : Contact */}
+        <div className="footer-links">
           <h3>Contact</h3>
-        </li>
-        <li>Adress</li>
-        <li>Phone Number</li>
-        <li>email</li>
-      </ul>
-      <ul>
-        <li>
-          <h3>Social Media Links</h3>
-        </li>
-        <li>Adress</li>
-        <li>Phone Number</li>
-        <li>email</li>
-      </ul>
+          <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+
+        {/* Fouth Column : Social Media */}
+        <div className="footer-links">
+          <h3>Social Media</h3>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+          </ul>
+        </div>
+
+      </div>
     </footer>
   );
 }
--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -1,12 +1,26 @@
 export default function Nav() {
-  return <nav>
-    <ul>
-        <li><a href="#HomePage">HomePage</a></li>
-        <li><a href="#About">About</a></li>
-        <li><a href="#Menu">Menu</a></li>
-        <li><a href="#Reservations">Reservations</a></li>
-        <li><a href="#OrderOnline">Order Online</a></li>
-        <li><a href="#Login">Login</a></li>
-    </ul>
-  </nav>;
+  return (
+    <nav>
+      <ul className="nav-links">
+        <li>
+          <a href="#Home">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Menu">Menu</a>
+        </li>
+        <li>
+          <a href="#Reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="#OrderOnline">Order Online</a>
+        </li>
+        <li>
+          <a href="#Login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
